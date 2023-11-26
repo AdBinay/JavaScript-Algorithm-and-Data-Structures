d36de5b8ed174058e5b88aa7242d990387536391
Binay77 <binayadh77@gmail.com>
2023-11-26 09:39:30 +0545

    FCC-JS-BASICS

---
+++ 01_Comment Your JavaScript Code.js
@@ -0,0 +1,11 @@
+                        // Comment Your JavaScript Code
+
+/* Comments are lines of code that JavaScript will intentionally ignore.
+Comments are a great way to leave notes to yourself and to other people
+ who will later need to figure out what that code does. */
+
+//  There are two ways to write comments in JavaScript:
+
+/* Using // will tell JavaScript to ignore the
+remainder of the text on the current line.
+This is an in-line comment: */
--- 02_Declare JavaScript Variables.js
+++ 02_Declare JavaScript Variables.js
@@ -1,6 +1,6 @@
-Declare JavaScript Variables
+// Declare JavaScript Variables
 
-In computer science, data is anything that is meaningful to the computer. JavaScript provides eight different data types which are undefined, null, boolean, string, symbol, bigint, number, and object.
+/*In computer science, data is anything that is meaningful to the computer. JavaScript provides eight different data types which are undefined, null, boolean, string, symbol, bigint, number, and object.
 
 For example, computers distinguish between numbers, such as the number 12, and strings, such as "12", "dog", or "123 cats", which are collections of characters. Computers can perform mathematical operations on a number, but not on a string.
 
@@ -11,6 +11,9 @@ Variables are similar to the x and y variables you use in mathematics, which mea
 We tell JavaScript to create or declare a variable by putting the keyword var in front of it, like so:
 
 var ourName;
-creates a variable called ourName. In JavaScript we end statements with semicolons. Variable names can be made up of numbers, letters, and $ or _, but may not contain spaces or start with a number.
+creates a variable called ourName. In JavaScript we end statements with semicolons. Variable names can be made up of numbers, letters, and $ or _, but may not contain spaces or start with a number.*/
 
-Use the var keyword to create a variable called myName.
+
+// Use the var keyword to create a variable called myName.
+
+var myName;
