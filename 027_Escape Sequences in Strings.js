ca0cba7192335cc974fff6bc54f66c408fc48537
Binay77 <binayadh77@gmail.com>
2023-11-27 20:12:52 +0545

    FCC-JS-BASICS

---
+++ 027_Escape Sequences in Strings.js
@@ -0,0 +1,26 @@
+// Escape Sequences in Strings
+
+/* Quotes are not the only characters that can be escaped inside a string. Escape sequences allow you to use characters you may not otherwise be able to use in a string.
+
+Code	Output
+\'	single quote
+\"	double quote
+\\	backslash
+\n	newline
+\t	tab
+\r	carriage return
+\b	backspace
+\f	form feed
+Note that the backslash itself must be escaped in order to display as a backslash.
+
+Assign the following three lines of text into the single variable myStr using escape sequences.
+
+FirstLine
+    \SecondLine
+ThirdLine
+You will need to use escape sequences to insert special characters correctly. You will also need
+to follow the spacing as it looks above, with no spaces between escape sequences or words.
+
+Note: The indentation for SecondLine is achieved with the tab escape character, not spaces. */
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
