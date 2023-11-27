f817b4ff403e74fc54b167e75682acfdb6f7e779
Binay77 <binayadh77@gmail.com>
2023-11-27 22:58:48 +0545

    FCC-JS-BASICS

---
+++ 031_Appending Variables to Strings.js
@@ -0,0 +1,18 @@
+//  Appending Variables to Strings
+
+/* Just as we can build a string over multiple lines out of string literals, we can also append variables to a string using the plus equals (+=) operator.
+
+Example:
+
+const anAdjective = "awesome!";
+let ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+ourStr would have the value freeCodeCamp is awesome!. */
+
+/* Set someAdjective to a string of at least 3
+characters and append it to myStr using the += operator. */
+
+// Change code below this line
+const someAdjective = "fun";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
