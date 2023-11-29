dcd2b4b750416f1cf8162641443d19e732f2e2fb
Binay77 <binayadh77@gmail.com>
2023-11-29 10:09:59 +0545

    FCC-JS-BASICS

---
+++ 071_Introducing Else Statements.js
@@ -0,0 +1,29 @@
+//  Introducing Else Statements
+
+/* When a condition for an if statement is true, the block of code following it is executed. What about when that condition is false? Normally nothing would happen. With an else statement, an alternate block of code can be executed.
+
+if (num > 10) {
+  return "Bigger than 10";
+} else {
+  return "10 or Less";
+} */
+
+//  Combine the if statements into a single if/else statement.
+
+function testElse(val) {
+    let result = "";
+    // Only change code below this line
+  
+    if (val > 5) {
+      result = "Bigger than 5";
+    }
+  
+    else {
+      result = "5 or Smaller";
+    }
+  
+    // Only change code above this line
+    return result;
+  }
+  
+testElse(4);
